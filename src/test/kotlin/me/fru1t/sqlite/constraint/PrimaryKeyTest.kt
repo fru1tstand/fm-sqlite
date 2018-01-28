8b71febd69cee6929675ce83e0cd99629da4daec
kodlee yin <kodlee@fru1t.me>
2018-01-28 13:34:35 -0800

    Add PrimaryKey constraint.
    Introduces the PrimaryKey constraint into the library. Fairly
    straightforward array of columns for the PrimaryKey. A follow-up CL will
    hook this into the TABLE CREATE statement.
    
    Test: ./gradlew clean test
    Issue: closes #1

---
+++ src/test/kotlin/me/fru1t/sqlite/constraint/PrimaryKeyTest.kt
@@ -0,0 +1,66 @@
+package me.fru1t.sqlite.constraint
+
+import com.google.common.truth.Truth.assertThat
+import me.fru1t.sqlite.Table
+import me.fru1t.sqlite.annotation.Column
+import me.fru1t.sqlite.annotation.DataType.INTEGER
+import org.junit.jupiter.api.Test
+
+class PrimaryKeyTest {
+  @Test
+  fun getConstraintClause() {
+    val primaryKey = PrimaryKey(arrayOf(TestTable::param1, TestTable::param2))
+    assertThat(primaryKey.getConstraintClause())
+        .isEqualTo("PRIMARY KEY(`param1`,`param2`)")
+  }
+
+  @Test
+  fun getConstraintClause_empty() {
+    val primaryKey = PrimaryKey<TestTable>(emptyArray())
+    assertThat(primaryKey.getConstraintClause()).isEmpty()
+  }
+
+  @Test
+  fun of() {
+    val result = PrimaryKey.of(TestTable::param1, TestTable::param2)
+    assertThat(result.columns).asList().containsExactly(TestTable::param1, TestTable::param2)
+  }
+
+  @Test
+  fun equals_valid() {
+    val primaryKey = PrimaryKey(arrayOf(TestTable::param1, TestTable::param2))
+    val otherPrimaryKey = PrimaryKey(arrayOf(TestTable::param1, TestTable::param2))
+    assertThat(primaryKey == otherPrimaryKey).isTrue()
+  }
+
+  @Test
+  fun equals_sameInstance() {
+    val primaryKey = PrimaryKey(arrayOf(TestTable::param1, TestTable::param2))
+    assertThat(primaryKey == primaryKey).isTrue()
+  }
+
+  @Test
+  fun equals_notSameClass() {
+    val primaryKey = PrimaryKey(arrayOf(TestTable::param1, TestTable::param2))
+    val other = arrayOf(TestTable::param1, TestTable::param2)
+    @Suppress("ReplaceCallWithComparison")
+    assertThat(primaryKey.equals(other)).isFalse()
+  }
+
+  @Test
+  fun equals_differentColumns() {
+    val primaryKey = PrimaryKey(arrayOf(TestTable::param1, TestTable::param2))
+    val otherPrimaryKey = PrimaryKey(arrayOf(TestTable::param1))
+    assertThat(primaryKey == otherPrimaryKey).isFalse()
+  }
+
+  @Test
+  fun hasCode_valid() {
+    val primaryKey = PrimaryKey(arrayOf(TestTable::param1, TestTable::param2))
+    val otherPrimaryKey = PrimaryKey(arrayOf(TestTable::param1, TestTable::param2))
+    assertThat(primaryKey.hashCode()).isEqualTo(otherPrimaryKey.hashCode())
+  }
+}
+
+private data class TestTable(@Column(INTEGER) val param1: Int, @Column(INTEGER) val param2: Int) :
+    Table<TestTable>()
