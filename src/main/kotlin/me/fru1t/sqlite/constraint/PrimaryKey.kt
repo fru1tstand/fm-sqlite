0d973b9ca967c0182b00dba721bd9b132fa7475c
kodlee yin <kodlee@fru1t.me>
2018-01-28 22:29:22 -0800

    Add official documentation to PrimaryKey.
    Expanded on the PrimaryKey data class KDoc with text from the official
    website.
    
    Test: none

--- src/main/kotlin/me/fru1t/sqlite/constraint/PrimaryKey.kt
+++ src/main/kotlin/me/fru1t/sqlite/constraint/PrimaryKey.kt
@@ -1,15 +1,13 @@
 package me.fru1t.sqlite.constraint
 
+import me.fru1t.sqlite.annotation.DataType
 import me.fru1t.sqlite.Table
 import java.util.Arrays
 import kotlin.reflect.KProperty1
 
 /**
  * Declares a [Table]'s [PrimaryKey] columns. Only a single primary key group may exist for a single
- * table. The name of the field is ignored in the creation of the table (as primary keys aren't
- * referenced directly anyway).
- *
- * Use [PrimaryKey.of] to create instances of this class.
+ * table. Use [PrimaryKey.of] to create instances of this class.
  *
  * Example usage:
  * ```
@@ -22,6 +20,38 @@ import kotlin.reflect.KProperty1
  *   }
  * }
  * ```
+ *
+ * Documentation from [https://www.sqlite.org/lang_createtable.html#constraints]
+ *
+ * Each table in SQLite may have at most one [`PRIMARY KEY`][PrimaryKey]. If the keywords
+ * [`PRIMARY KEY`][PrimaryKey] are added to a column definition, then the primary key for the table
+ * consists of that single column. Or, if a [`PRIMARY KEY`][PrimaryKey] clause is specified as a
+ * table-constraint, then the primary key of the table consists of the list of columns specified as
+ * part of the [`PRIMARY KEY`][PrimaryKey] clause. The [`PRIMARY KEY`][PrimaryKey] clause must
+ * contain only column names — the use of expressions in an indexed-column of a
+ * [`PRIMARY KEY`][PrimaryKey] is not supported. An error is raised if more than one
+ * [`PRIMARY KEY`][PrimaryKey] clause appears in a `CREATE TABLE` statement. The
+ * [`PRIMARY KEY`][PrimaryKey] is optional for ordinary tables but is required for `WITHOUT ROWID`
+ * tables.
+ *
+ * If a table has a single column primary key and the declared type of that column is
+ * "[`INTEGER`][DataType.INTEGER]" and the table is not a `WITHOUT ROWID` table, then the column is
+ * known as an `INTEGER PRIMARY KEY`. See below for a description of the special properties and
+ * behaviors associated with an `INTEGER PRIMARY KEY`.
+ *
+ * Each row in a table with a primary key must have a unique combination of values in its primary
+ * key columns. For the purposes of determining the uniqueness of primary key values, `NULL` values
+ * are considered distinct from all other values, including other `NULL`s. If an `INSERT` or
+ * `UPDATE` statement attempts to modify the table content so that two or more rows have identical
+ * primary key values, that is a constraint violation.
+ *
+ * According to the SQL standard, [`PRIMARY KEY`][PrimaryKey] should always imply `NOT NULL`.
+ * Unfortunately, due to a bug in some early versions, this is not the case in SQLite. Unless the
+ * column is an `INTEGER PRIMARY KEY` or the table is a `WITHOUT ROWID` table or the column is
+ * declared `NOT NULL`, SQLite allows `NULL` values in a `PRIMARY KEY` column. SQLite could be fixed
+ * to conform to the standard, but doing so might break legacy applications. Hence, it has been
+ * decided to merely document the fact that SQLite allowing `NULL`s in most
+ * [`PRIMARY KEY`][PrimaryKey] columns.
  */
 data class PrimaryKey<T : Table<T>>(val columns: Array<out KProperty1<T, *>>) {
   /**
