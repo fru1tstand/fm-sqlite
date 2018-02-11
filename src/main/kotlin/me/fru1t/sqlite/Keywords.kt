a4ffe051352d614153c498ba7be3741911579742
kodlee yin <kodlee@fru1t.me>
2018-02-10 16:32:56 -0800

    Add Order enum.
    Renamed DataType.kt -> Keywords.kt and added Order enum to the file.
    
    Test: None
    Issue: #11

--- src/main/kotlin/me/fru1t/sqlite/Keywords.kt
+++ src/main/kotlin/me/fru1t/sqlite/Keywords.kt
@@ -17,3 +17,22 @@ enum class DataType(val value: String) {
   /** Everything else. Usually a fixed decimal point field, but that's currently unsupported. */
   NUMERIC("NUMERIC")
 }
+
+/** Direction of data that can be used in an `ORDER BY` or `UNIQUE` clause. */
+enum class Order(val value: String) {
+  /**
+   * Ascending order: starting from the lowest value going to the highest.
+   *
+   * Lexicographically: (0 -> 9) -> (A -> Z)
+   * Numerically: -Infinity -> Infinity
+   */
+  ASC("ASC"),
+
+  /**
+   * Descending order: starting from the highest value going to the lowest.
+   *
+   * Alphabetically: (Z -> A) -> (9 -> 0)
+   * Numerically: Infinity -> -Infinity
+   */
+  DESC("DESC")
+}
