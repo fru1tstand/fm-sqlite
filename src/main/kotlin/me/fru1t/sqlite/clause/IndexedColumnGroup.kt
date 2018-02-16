84bb45ececc62118c4fe65668e35bcb62243bafe
kodlee yin <kodlee@fru1t.me>
2018-02-15 23:13:38 -0800

    Remove IndexedColumnGroupClause
    Instead of handling both IndexedColumn and IndexedColumnGroup, only
    IndexedColumnGroup will be exposed as a vector for handling the
    indexed-column clause. This means the IndexedColumn data class is simply
    a container class with little logic. The IndexedColumnGroup class acts
    like the entire clause (even holding only a single IndexedColumn if need
    be). This reduces the code complexity and burden for both users of the
    API and the API itself to only need to accept the IndexedColumnGroup
    type.
    
    Test: ./gradlew clean test
    Issue: none

---
+++ src/main/kotlin/me/fru1t/sqlite/clause/IndexedColumnGroup.kt
@@ -0,0 +1,64 @@
+package me.fru1t.sqlite.clause
+
+import me.fru1t.sqlite.Clause
+import me.fru1t.sqlite.Order
+import me.fru1t.sqlite.TableColumns
+import me.fru1t.sqlite.getSqlName
+import kotlin.reflect.KProperty1
+
+/** Creates an [IndexedColumnGroup] with a single [ordered][Order] [IndexedColumn]. */
+infix fun <T : TableColumns<T>> KProperty1<T, *>.order(order: Order): IndexedColumnGroup<T> =
+  IndexedColumnGroup(IndexedColumn(this, order))
+
+/** Creates an [IndexedColumnGroup] by appending the [nextColumn] to this column. */
+infix fun <T : TableColumns<T>> KProperty1<T, *>.and(
+    nextColumn: KProperty1<T, *>): IndexedColumnGroup<T> =
+  IndexedColumnGroup(listOf(IndexedColumn(this), IndexedColumn(nextColumn)))
+
+/** Creates an [IndexedColumnGroup] by prepending this column to the [existingGroup]. */
+infix fun <T : TableColumns<T>> KProperty1<T, *>.and(
+    existingGroup: IndexedColumnGroup<T>): IndexedColumnGroup<T> =
+  IndexedColumnGroup(listOf(IndexedColumn(this), *existingGroup.columns.toTypedArray()))
+
+/**
+ * A simple container class that represents a single
+ * [`indexed-column`][https://www.sqlite.org/syntax/indexed-column.html]. This data class holds a
+ * [column] and optionally an [order].
+ */
+data class IndexedColumn<T : TableColumns<T>>(
+    val column: KProperty1<T, *>, val order: Order? = null) {
+
+  /** Outputs the [column] sql name. Example: `` `foo` ``. */
+  fun toStringWithoutOrder(): String = "`${column.getSqlName()}`"
+
+  /** Outputs the [column] sql name and [order] if available. Example: `` `foo` ASC ``. */
+  override fun toString(): String =
+    "`${column.getSqlName()}`" + (order?.let { " ${it.value}" } ?: "")
+}
+
+/**
+ * Represents the repeated [`indexed-column`][https://www.sqlite.org/syntax/indexed-column.html]
+ * clause. The base use case for these are the `UNIQUE` and `PRIMARY KEY` constraints, but may other
+ * column-grouping applications.
+ */
+data class IndexedColumnGroup<T : TableColumns<T>>(val columns: List<IndexedColumn<T>>) : Clause {
+  /** Creates an [IndexedColumnGroup] from an initial column. */
+  constructor(initialColumn: KProperty1<T, *>) : this(IndexedColumn(initialColumn))
+
+  /** Creates an [IndexedColumnGroup] from an initial [IndexedColumn]. */
+  constructor(initialColumn: IndexedColumn<T>) : this(listOf(initialColumn))
+
+  /** Example: ``(`post_id`,`name`,`email`)``. */
+  fun getClauseWithoutOrder(): String = "(${columns.joinToString { it.toStringWithoutOrder() }})"
+
+  /** Example: ``(`post_id` DESC,`name` ASC,`email`)``. */
+  override fun getClause(): String = "(${columns.joinToString()})"
+
+  /** Adds another [IndexedColumnGroup] to the end of this [IndexedColumnGroup]. */
+  infix fun and(indexedColumnGroup: IndexedColumnGroup<T>): IndexedColumnGroup<T> =
+    copy(columns = listOf(*columns.toTypedArray(), *indexedColumnGroup.columns.toTypedArray()))
+
+  /** Adds a [column] to the end of this [IndexedColumnGroup]. */
+  infix fun and(column: KProperty1<T, *>): IndexedColumnGroup<T> =
+    copy(columns = listOf(*columns.toTypedArray(), IndexedColumn(column)))
+}
---
+++ src/test/kotlin/me/fru1t/sqlite/clause/IndexedColumnGroupTest.kt
@@ -0,0 +1,99 @@
+package me.fru1t.sqlite.clause
+
+import com.google.common.truth.Truth.assertThat
+import me.fru1t.sqlite.Order.DESC
+import me.fru1t.sqlite.TableColumns
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class FileTest {
+  @Test
+  fun kProperty1_order() {
+    val result = Table::a order DESC
+    assertThat(result.columns).hasSize(1)
+    assertThat(result.columns[0].column).isEqualTo(Table::a)
+    assertThat(result.columns[0].order).isEqualTo(DESC)
+  }
+
+  @Test
+  fun kProperty1_and_kProperty1() {
+    val result = Table::a and Table::b
+    assertThat(result.columns).containsExactly(IndexedColumn(Table::a), IndexedColumn(Table::b))
+  }
+
+  @Test
+  fun kProperty1_and_indexedColumn() {
+    val result = Table::a and (Table::b order DESC)
+    assertThat(result.columns)
+        .containsExactly(IndexedColumn(Table::a), IndexedColumn(Table::b, DESC))
+  }
+}
+
+class IndexedColumnTest {
+  private lateinit var indexedColumn: IndexedColumn<Table>
+
+  @BeforeEach
+  fun setUp() {
+    indexedColumn = IndexedColumn(Table::a, DESC)
+  }
+
+  @Test
+  fun overrideToString() {
+    assertThat(indexedColumn.toString()).isEqualTo("`a` DESC")
+  }
+
+  @Test
+  fun toStringWithoutOrder() {
+    assertThat(indexedColumn.toStringWithoutOrder()).isEqualTo("`a`")
+  }
+}
+
+class IndexedColumnGroupTest {
+  private lateinit var indexedColumnGroup: IndexedColumnGroup<Table>
+
+  @BeforeEach
+  fun setUp() {
+    indexedColumnGroup =
+        IndexedColumnGroup(listOf(IndexedColumn(Table::a), IndexedColumn(Table::b, DESC)))
+  }
+
+  @Test
+  fun constructor_kProperty1() {
+    val result = IndexedColumnGroup(Table::a)
+    assertThat(result.columns).containsExactly(IndexedColumn(Table::a))
+  }
+
+  @Test
+  fun constructor_indexedColumn() {
+    val result = IndexedColumnGroup(IndexedColumn(Table::a, DESC))
+    assertThat(result.columns).containsExactly(IndexedColumn(Table::a, DESC))
+  }
+
+  @Test
+  fun getClauseWithoutOrder() {
+    assertThat(indexedColumnGroup.getClauseWithoutOrder()).isEqualTo("(`a`, `b`)")
+  }
+
+  @Test
+  fun getClause() {
+    assertThat(indexedColumnGroup.getClause()).isEqualTo("(`a`, `b` DESC)")
+  }
+
+  @Test
+  fun and_indexedColumn() {
+    val result = indexedColumnGroup and (Table::c order DESC)
+    assertThat(result.columns)
+        .containsExactly(
+            IndexedColumn(Table::a), IndexedColumn(Table::b, DESC), IndexedColumn(Table::c, DESC))
+  }
+
+  @Test
+  fun and_kProperty1() {
+    val result = indexedColumnGroup and Table::c
+    assertThat(result.columns)
+        .containsExactly(
+            IndexedColumn(Table::a), IndexedColumn(Table::b, DESC), IndexedColumn(Table::c))
+  }
+}
+
+private data class Table(val a: Int, val b: String, val c: String) : TableColumns<Table>()
